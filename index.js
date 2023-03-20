a3f04c32ef089b1286b837cadc7b71cf1b32be72
J2mCoder <jeandedieumbumba2802@gmail.com>
2023-03-20 02:27:26 +0100

    jD 2023-03-20T02:27:26+01:00

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const jsonfile = require('jsonfile')
 const moment = require('moment')
 const simpleGit = require('simple-git')
-
 const FILE_PATH = './data.json'
 
-const makecommits = (x, y) => {
+const makecommits = n => {
+  if (n === 0) return simpleGit().push()
+  const x = Math.floor(Math.random() * 55)
+  const y = Math.floor(Math.random() * 6)
   const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format()
   const date = {
     date: DATE
@@ -12,15 +14,11 @@ const makecommits = (x, y) => {
   jsonfile.writeFile(FILE_PATH, date, () => {
     const git = simpleGit();
     git.add('./*')
-      .commit("jD " + DATE, { '--date': DATE })
-      .push()
+      .commit("jD " + DATE, { '--date': DATE }, makecommits.bind(this, --n))
   })
 }
 
-makecommits(3, 4)
-makecommits(3, 4)
-makecommits(3, 4)
-makecommits(3, 4)
+makecommits(100)
 
 
 
